04b40f22cca1b1334849b76f1c160303b08c8af1
Ben Hanna <benpaulhanna@gmail.com>
2016-07-26 20:48:01 +0000

    Fixed options for remove and update generators.

--- generators/remove.js
+++ generators/remove.js
@@ -7,7 +7,8 @@ function init(model) {
         var options = this.state.options || {},
             primaryKey = model.primaryKeyAttribute;
             
-        options[primaryKey] = this.params[primaryKey];
+        options.where = options.where || {};
+        options.where[primaryKey] = this.params[primaryKey];
         
         var result = yield model.findOne(options);
         
